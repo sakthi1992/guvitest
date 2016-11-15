c9d32d7003ff9470e52edb1a7b752e93d41f7d2a
sakthi1992 <sakthikumar.it@gmail.com>
2016-11-15 16:14:35 +0530

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,21 @@
+import math
+
+_author_ = 'sakthi'
+
+class ex1:
+    def demo(self):
+
+
+
+                a = int(input("please enter a value "))
+
+                if a == 0:
+                    print("you are entered 0")
+                elif a > 0:
+                    print ("you ar eentered positive number")
+                elif a < 0:
+                    print ("you are entered negative number")
+
+ex1().demo()
+
+
