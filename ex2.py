cd8bc9e9fcff4d5e7468cd06964337b3567bfab3
sakthi1992 <sakthikumar.it@gmail.com>
2016-11-15 16:27:27 +0530

    Create ex2.py

---
+++ ex2.py
@@ -0,0 +1,15 @@
+import math
+
+_author_ = 'sakthi'
+
+class ex2:
+    def demo(self):
+
+                a = int(input("please enter a value "))
+                if a % 2:
+                    print("odd number")
+                else:
+                    print ("even number")
+
+
+ex2().demo()
